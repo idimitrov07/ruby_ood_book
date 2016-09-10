e2ef3622c9efae663a2b2cc763808d0ef031f90c
Ivan Dimitrov <idimitrov07@gmail.com>
2016-09-10 17:07:48 +0300

    Start chapter 3 - managing dependencies.

---
+++ chapter_3/gear_modified.rb
@@ -0,0 +1,37 @@
+# modified gear class
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+end
+
+# modified wheel class
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+puts Gear.new(52, 11, 26, 1.5).gear_inches
