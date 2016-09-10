53f46f4d77817ccaedc17a8a3c9ec338d0be5544
Ivan Dimitrov <idimitrov07@gmail.com>
2016-09-10 17:58:12 +0300

    Start decoupling classes - dependencies injection.

---
+++ chapter_3/gear_first_edit.rb
@@ -0,0 +1,37 @@
+# start modifying gear class
+# Instead of being glued to Wheel, this next version of Gear expects to be initialized with an object that can respond to diameter
+class Gear
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel)
+    @chainring = chainring
+    @cog = cog
+    @wheel = wheel
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+
+  def gear_inches
+    ratio * wheel.diameter
+  end
+end
+
+# old wheel class
+class Wheel
+  attr_reader :rim, :tire
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+puts Gear.new(52, 11, Wheel.new(26, 1.5)).gear_inches
